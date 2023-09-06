dedff23efec51190a98ba1886d1102b6a36d4d6a
Csaba Farago <farago.csaba.phd@gmail.com>
2023-09-06 20:08:06 +0200

    Implement subtract

--- src/main/java/hu/faragocsaba/mymath/MyMath.java
+++ src/main/java/hu/faragocsaba/mymath/MyMath.java
@@ -1,7 +1,11 @@
 package hu.faragocsaba.mymath;
 
-public class MyMath {	
+public class MyMath {
 	public static int myAdd(int a, int b) {
 		return a + b;
+	}
+
+	public static int mySubtract(int a, int b) {
+	    return a-b;
 	}
 }
